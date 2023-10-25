30852c6668f33076b9d6de487bc2521efa81fac3
Nick A. Spyrakos <nickpreveza@gmail.com>
2023-10-25 21:41:43 +0200

    Trying XR Toolkit and native Unity solutions

---
+++ Assets/HexCore.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HexCore : MonoBehaviour
+{
+    //ref object
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+public enum ObjectType
+{
+    Unit1,
+    Unit2,
+    Unit3,
+    Unit4,
+    Tower1,
+    Tower2,
+    House
+}
